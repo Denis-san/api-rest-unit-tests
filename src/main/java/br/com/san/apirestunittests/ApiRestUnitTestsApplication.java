a251b0fdac18ce07c5feafa691b31aa0b144c0cb
Denis-san <denilson.nt.nc@gmail.com>
2024-04-06 19:01:22 -0300

    initial commit - project created

---
+++ src/main/java/br/com/san/apirestunittests/ApiRestUnitTestsApplication.java
@@ -0,0 +1,13 @@
+package br.com.san.apirestunittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestUnitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestUnitTestsApplication.class, args);
+	}
+
+}
